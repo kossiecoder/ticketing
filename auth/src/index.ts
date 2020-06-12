d541a372e0850419a735576aa71f4cf0309cb2a7
Kossie Coder <kossie.coder@gmail.com>
2020-06-12 11:26:43 +1000

    mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +18,27 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-    console.log('on 3000!');
-});
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+})
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('connected to mongodb')
+    } catch (e) {
+        console.log(e);
+    }
+
+    app.listen(3000, () => {
+        console.log('on 3000!');
+    });
+};
+
+start();
+
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import { User } from '../models/user';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -7,18 +10,24 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20})
         .withMessage('password must be between 4 and 20')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array());
+        throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    console.log('creating a user');
 
-    res.send({});
+    const existingUser = await User.findOne({ email });
 
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+    }
 
-    //
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
+
 });
 
 export { router as signupRouter };
